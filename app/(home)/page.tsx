cf1d735a93105a78d17bd74b205979a7bf3cb659
hshim-stack <hshim@ualberta.ca>
2026-04-02 10:12:39 -0600

    update

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,17 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    );
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+export const metadata = {
+    title: "Home",
+};
+
+async function getMovies() {
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+
+export default async function HomePage() { // async function을 사용하면  await 키워드를 사용할 수 있다.
+    const movies = await getMovies();
+    return <h1>{JSON.stringify(movies)}</h1>;
 }
 
--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -1,4 +1,5 @@
 export default function MovieDetail() {
 
     return <h1>Movie Detail</h1>;
+
 }
